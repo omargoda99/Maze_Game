f97641a119b390aad19aefa24d997f6444c8b708
Muhammad H. Bakr <150179045+M-H-Bakr@users.noreply.github.com>
2023-12-11 19:20:11 +0200

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -68,7 +68,12 @@ public class Menu extends JFrame {
 @SuppressWarnings({"ThrowablePrintedToSystemOut", "CallToPrintStackTrace"})
 class GLMenu implements GLEventListener, MouseListener {
 
-
+//Canvas Coordinates:
+    double xCanvas = 0, yCanvas = 0;
+    //Scores and info:
+    StringBuilder players_Scores = new StringBuilder();
+    LinkedHashMap<String, Long> Scores;
+    
     //Textures:
     String[] textureNames = {"black-square-clipart-8.png", "R (1).png",
             "InShot_20231205_163202992.png", "InShot_20231205_163220184.png",
@@ -191,7 +196,188 @@ class GLMenu implements GLEventListener, MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
+    double x = e.getX();
+        double y = e.getY();
+        Component c = e.getComponent();
+        double width = c.getWidth();
+        double height = c.getHeight();
+        /* Eq to switch from JFrame with 0 <= x <= width, and 0 <= y <= height
+        to GLCanvas with -x` <= xc <= x`, and -y` <= yc <= y` is:
+        xc = (2*x*x`/width)-x`, and yc = y`-(2*y*y`/height). */
+        xCanvas = (2 * x / width) - 1;
+        yCanvas = 1 - (2 * y / height);
+        if (Variables.menu_disabled) {
+            xCanvas = 0;
+            yCanvas = 0;
+        }
+        if (xCanvas <= 0.66 && xCanvas >= 0.36 && yCanvas >= 0.25 && yCanvas <= 0.33) {
+            //Modes:
+            if (!Variables.selected_Mode) {
+                Variables.selected_Mode = true;
+                Variables.Num_Players = 1;
+            } else {
+                //Levels:
+                Variables.levelSelection = 0;
+                Variables.player1_Lives = 3;
+                Variables.max_Minutes = 3;
+                if (Variables.Num_Players == 2) {
+                    Variables.player2_Lives = 3;
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                    Variables.player2 = JOptionPane.showInputDialog("Player2:");
+                } else if (Variables.Num_Players == 1) {
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                }
+            }
+        } else if (xCanvas <= 0.66 && xCanvas >= 0.36 && yCanvas >= 0.066 && yCanvas <= 0.133) {
+            if (!Variables.selected_Mode) {
+                //Modes:
+                Variables.selected_Mode = true;
+                Variables.Num_Players = 2;
+            } else {
+                //Levels:
+                Variables.levelSelection = 1;
+                Variables.player1_Lives = 2;
+                Variables.max_Minutes = 2;
+                if (Variables.Num_Players == 2) {
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                    Variables.player2 = JOptionPane.showInputDialog("Player2:");
+                    Variables.player2_Lives = 2;
+                } else if (Variables.Num_Players == 1) {
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                }
+            }
+        } else if (xCanvas <= 0.66 && xCanvas >= 0.36 && yCanvas >= -0.133 && yCanvas <= -0.066) {
+            if (!Variables.selected_Mode) {
+                //Modes:
+                Variables.selected_Mode = true;
+                Variables.selectedAI = true;
+                Variables.Num_Players = 1;
+            } else {
+                //Levels:
+                Variables.levelSelection = 2;
+                Variables.player1_Lives = 1;
+                Variables.max_Minutes = 1;
+                if (Variables.Num_Players == 2) {
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                    Variables.player2 = JOptionPane.showInputDialog("Player2:");
+                    Variables.player2_Lives = 1;
+                } else if (Variables.Num_Players == 1) {
+                    Variables.player1 = JOptionPane.showInputDialog("Player1:");
+                }
+            }
+        } else if (xCanvas <= 0.66 && xCanvas >= 0.36 && yCanvas >= -0.33 && yCanvas <= -0.25) {
+            //Instructions:
+            JOptionPane.showMessageDialog(null, """
+                            The_Maze is a 2D game consisting of 3 level selections:
+                                                        
+                            Easy, Normal, and Hard with 3 different rounds for each.
+                                                        
+                            Three modes: Single, Multi, and Vs AI.
+                                                        
+                            In the beginning you get trapped in the initial position.
+                                                        
+                            The Ultimate objective (Winning) is to capture the star at the end of the maze.
+                                                        
+                            Losing conditions: Wasted lives by collision with walls, Reaching max time,
+                            Or not being the first to capture the star (Applies only in Vs AI and Multi).
+                                                        
+                            If a player died in a multiplayer game the maze becomes unsafe (Red)
+                            Hence making it hard to know when a collision happens for the other player
+                            the maze also becomes unsafe if a collision happened until the player who
+                            collided moves correctly, also in multi player:
+                            The player ending the last round first is considered the winner.
+                                                        
+                            A wall randomly appears every 2 seconds as a trap
+                            (Not necessarily at the first step when playing VS AI)
+                            so be careful.
+                                
+                            You're given for the modes: Easy, Medium, Hard...
+                            Max time (in minutes until the game ends) as follows:
+                            3, 2, and 1, also lives (until the game ends)
+                            3, 2 and 1 all respectively.
+                                                        
+                            Score is calculated for each round as follows:
+                            Seconds remaining of total time * 100 + Lives remaining * 100.
+                                 
+                            When playing against AI it moves a step every second.
+                            In order to win Against it: you have to be as fast and as sharp...
+                            As the AI moves correctly every time so it will eventually win
+                            if you didn't do it first.
+                                                        
+                            Controls: for player1 WASD, for player2  IJKL that is Up Left Down Right respectively.
+                                         
+                                                        
+                            Happy Gaming!
+                                                        
+                            Game Created by: Muhammad H. Bakr, Kareem Amin, Omar Goda, and Muhammad Ashraf."""
+                    ,
+                    "INSTRUCTIONS", JOptionPane.INFORMATION_MESSAGE);
+        } else if (xCanvas <= -0.4 && x >= -0.6 && yCanvas >= 0.6 && yCanvas <= 0.8) {
+            //Mute:
+            if (!Variables.muted) {
+                Variables.muted = true;
+                Variables.clipTime = Variables.clip.getMicrosecondPosition();
+                Variables.clip.stop();
+            } else {
+                //Unmute:
+                Variables.muted = false;
+                Variables.clip.setMicrosecondPosition(Variables.clipTime);
+                Variables.clip.start();
+            }
+        } else if (yCanvas <= -0.62 && yCanvas >= -0.78 && xCanvas <= -0.17 && xCanvas >= -0.42) {
+            //HighScores Easy:
+            Scores = Variables.HighScores_Easy.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+            for (Map.Entry<String, Long> entry : Scores.entrySet()) {
+                players_Scores.append("Player: ").append(entry.getKey()).append(", Score: ").append(entry.getValue()).append(".\n");
+            }
+            JOptionPane.showMessageDialog(null, players_Scores, "High Scores", JOptionPane.INFORMATION_MESSAGE);
+            while (!players_Scores.isEmpty()) {
+                players_Scores.deleteCharAt(0);
+            }
+        } else if (yCanvas <= -0.62 && yCanvas >= -0.78 && xCanvas >= -0.12 && xCanvas <= 0.12) {
+            //HighScores Normal:
+            Scores = Variables.HighScores_Normal.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+            for (Map.Entry<String, Long> entry : Scores.entrySet()) {
+                players_Scores.append("Player: ").append(entry.getKey()).append(", Score: ").append(entry.getValue()).append(".\n");
+            }
+            JOptionPane.showMessageDialog(null, players_Scores, "High Scores", JOptionPane.INFORMATION_MESSAGE);
+            while (!players_Scores.isEmpty()) {
+                players_Scores.deleteCharAt(0);
+            }
+        } else if (yCanvas <= -0.62 && yCanvas >= -0.78 && xCanvas >= 0.16 && xCanvas <= 0.42) {
+            //HighScores Hard:
+            Scores = Variables.HighScores_Hard.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+            for (Map.Entry<String, Long> entry : Scores.entrySet()) {
+                players_Scores.append("Player: ").append(entry.getKey()).append(", Score: ").append(entry.getValue()).append(".\n");
+            }
+            JOptionPane.showMessageDialog(null, players_Scores, "High Scores", JOptionPane.INFORMATION_MESSAGE);
+            while (!players_Scores.isEmpty()) {
+                players_Scores.deleteCharAt(0);
+            }
+        }
+        if (Variables.levelSelection > -1) {
+            //initialize the game frame:
+            gameFrame.game = new gameFrame();
+            Variables.menu_disabled = true;
+        }
     }
 
     @Override
